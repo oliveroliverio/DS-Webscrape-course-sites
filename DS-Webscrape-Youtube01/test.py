b43c8f905a462373c4077e487ab95c4d020f5841
Oliver Oliverio <oliveroliverio@gmail.com>
2021-11-20 23:10:25 -0800

    20211120 attempted webscraping courseupload site to get dload links

--- DS-Webscrape-Youtube01/test.py
+++ DS-Webscrape-Youtube01/test.py
@@ -7,13 +7,40 @@ import requests
 # print(htmlTxt)
 # getting a <response [503]> service unavaiable error
 
+# url = 'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation='
+url = 'https://courseupload.net/advanced-stock-trading-course-strategies-freedow200321/'
 
 
-htmlTxt = requests.get("https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=")
-# print(htmlTxt)
+htmlTxt = requests.get(url)
+print(htmlTxt)
 # getting <Response [200]>
 # [the requst has succeeded](https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html)
+
 soup = BeautifulSoup(htmlTxt.text, 'html.parser')
-job = soup.find("li", class_ = 'joblist-comp-name')
-print(job)
+# [element.text for s in soup.find_all('a', {"class": "responsiveInfoTable"}) for element in s.findChildren('href')]
+
+
+# subcat = soup.find_all('a', {"class": "responsiveInfoTable"})[x].findChildren()
+# for i, child in enumerate(subcat):
+#     categories = child.text
+#     print(categories)
+
+# This actually did something
+job = soup.find_all("a", class_ = 'responsiveInfoTable')
+
+# --------originals---------
+# def cattest():
+#     subcat = soup.find_all('span', {"class": "zg_hrsr_ladder"})[x].findChildren()
+#     for i, child in enumerate(subcat):
+#         categories = child.text
+#         print(categories)
+# res = []
+# for s in soup.find_all('a', {"class": "responsiveInfoTable"}):
+#   for element in s.findChildren('href'):
+#     res.append(element.text)
+
+# print(res)
+
+# job = soup.find_all("a", class_ = 'responsiveInfoTable')
+
 
